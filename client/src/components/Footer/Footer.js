ac3f4205f2ccd7c50177378da94e098b815c1eca
Mihail Tomov <62585647+mihailtomov@users.noreply.github.com>
2021-03-15 16:18:02 +0200

    Created initial project architecture
    Added Header, Main and Footer components as a basis to build upon. Removed some unnecessary files.

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>All rights reserved. &copy;</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,16 @@
+const Main = () => {
+    return (
+        <main>
+            <h2>All posts</h2>
+            <section>
+                <div className="post-container">
+                    <h3>Post heading</h3>
+                    <img src="http://blogorrah.com/wp-content/uploads/2017/02/blog.jpg" alt="" />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis similique ducimus laudantium hic numquam eveniet tempore eius ipsam architecto unde.</p>
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default Main;
