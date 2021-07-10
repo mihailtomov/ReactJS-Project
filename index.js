b7f5415a0b06e5d1fad66296430786520e849c08
Mihail Tomov <62585647+mihailtomov@users.noreply.github.com>
2021-07-10 15:54:21 +0300

    Path fixing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require('path');
 const app = express();
 require('./config/mongoose');
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(cors({ maxAge: 600 }));
 app.use(express.json());
 
--- routes.js
+++ routes.js
@@ -9,7 +9,7 @@ router.use('/auth', authController);
 router.use('/articles', articlesController);
 router.use('/users', usersController);
 router.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
 module.exports = router;
